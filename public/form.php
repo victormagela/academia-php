bc982a20ce61d0a8881cb3255b2cacaff6e8d5f5
Victor Mateus Magela Amato Ferreira <victormateusamato@outlook.com>
2026-05-10 19:25:29 -0300

    Add and style a container for the form and heading of the page

--- public/form.php
+++ public/form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,31 +9,37 @@
     <script src="./scripts/script.js"></script>
     <title>Orange Fit - Inscrição</title>
 </head>
+
 <body>
     <?php include './includes/header.html'; ?>
 
-    <form action="result.php" method="post">
-        <label for="name">Nome:</label>
-        <input type="text" id="name" name="name" required>
+    <div class="container">
+        <h2>Inscrição</h2>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+        <form action="result.php" method="post">
+            <label for="name">Nome:</label>
+            <input type="text" id="name" name="name" required>
 
-        <label for="phone">Telefone:</label>
-        <input type="tel" id="phone" name="phone" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
 
-        <label for="birth">Data de Nascimento:</label>
-        <input type="date" id="birth" name="birth" required>
+            <label for="phone">Telefone:</label>
+            <input type="tel" id="phone" name="phone" required>
 
-        <label for="cpf">CPF:</label>
-        <input type="text" id="cpf" name="cpf" required>
+            <label for="birth">Data de Nascimento:</label>
+            <input type="date" id="birth" name="birth" required>
 
-        <label for="adress">Endereço:</label>
-        <input type="text" id="adress" name="adress" required>
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf" required>
 
-        <input type="submit" value="Inscrever-se" name="submit">
-    </form>
+            <label for="adress">Endereço:</label>
+            <input type="text" id="adress" name="adress" required>
+
+            <input type="submit" value="Inscrever-se" name="submit">
+        </form>
+    </div>
 
     <?php include './includes/footer.html'; ?>
 </body>
+
 </html>
