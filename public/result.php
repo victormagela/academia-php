7876000610dd0ad4ed38befb7394a495dbb7826d
Victor Mateus Magela Amato Ferreira <victormateusamato@outlook.com>
2026-05-10 22:58:59 -0300

    Implemented the result.php page

--- public/result.php
+++ public/result.php
@@ -1,3 +1,29 @@
+<?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_POST['submit']) {
+            $data = [
+                "Nome" => $_POST['name'],
+                "Email" => $_POST['email'],
+                "Telefone" => $_POST['phone'],
+                "Data de Nascimento" => $_POST['birth'],
+                "CPF" => $_POST['cpf'],
+                "Endereço" => $_POST['adress']
+            ];
+
+            $content = '';
+            foreach ($data as $key => $value) {
+                $content .= "$key: $value\n";
+            }
+
+            $file_name = "inscrição_" . date("Ymd_His") . ".txt";
+            file_put_contents($file_name, $content);
+        }
+    } else {
+        // Redireciona para o formulário se o acesso for direto
+        header("Location: form.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,6 +34,18 @@
     <title>Resultado</title>
 </head>
 <body>
+    <?php include './includes/header.html'; ?>
+
+    <main>
+        <div class="container">
+            <?php if (isset($file_name)): ?>
+                <h2>Resultado</h2>
+                <p style="color: green; text-align: center;">Arquivo salvo como: <?= $file_name; ?></p>
+            <?php endif; ?>
+        </div>
+    </main>
     
+    <?php include './includes/footer.html'; ?>
+
 </body>
 </html>
