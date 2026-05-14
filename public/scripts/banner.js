dad070bfef9d02a646bfc30a195b4f06811492fc
Victor Mateus Magela Amato Ferreira <victormateusamato@outlook.com>
2026-05-14 15:28:37 -0300

    Fix the path to the images in banner.js and the suffix in the header anchor in header.html

--- public/scripts/banner.js
+++ public/scripts/banner.js
@@ -3,15 +3,15 @@ const credits = document.getElementById('credits');
 
 const banners = [
     {
-        image: '../assets/banner1.jpg',
+        image: './assets/banner1.jpg',
         credits: 'Foto de <span class="highlight-word"><a href="https://unsplash.com/pt-br/@dncerullo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Danielle Cerullo</a></span> na <span class="highlight-word"><a href="https://unsplash.com/pt-br/fotografias/mulher-em-pe-cercada-por-equipamento-de-exercicio-CQfNt66ttZM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></span>',
     },
     {
-        image: '../assets/banner2.jpg',
+        image: './assets/banner2.jpg',
         credits: 'Foto de <span class="highlight-word"><a href="https://unsplash.com/pt-br/@risennnnn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Risen Wang</a></span> na <span class="highlight-word"><a href="https://unsplash.com/pt-br/fotografias/homem-fazendo-levantamento-de-peso-CQfNt66ttZM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></span>',
     },
     {
-        image: '../assets/banner3.jpg',
+        image: './assets/banner3.jpg',
         credits: 'Foto de <span class="highlight-word"><a href="https://unsplash.com/pt-br/@victorfreitas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Victor Freitas</a></span> na <span class="highlight-word"><a href="https://unsplash.com/pt-br/fotografias/homem-fazendo-levantamento-de-peso-CQfNt66ttZM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></span>',
     }
 ];
